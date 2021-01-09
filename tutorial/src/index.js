15d536410029d29cf32ba04b8780c27dd39e3771
SuvamChanda2020 <suvam.net001@gmail.com>
2021-01-09 14:18:52 +0530

    Update Component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
+// This function is returning JSX and using React 
 function Greeting(){
   return <h4>this is Suvam and this is my first Component</h4>
 }
