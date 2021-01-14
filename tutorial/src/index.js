5798c12675bc2df1bf1cf255c0f53bd11547ecb6
SuvamChanda2020 <suvam.net001@gmail.com>
2021-01-14 08:30:39 +0530

    List in component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -13,34 +13,28 @@ import "./index.css";
 
 //Setup variables
 
-const firstBook = {
-  title: "Taranath Tantrik",
-  author: "Taradas Bandopadhyay, Bibhutibhushan Bandopadhyay, Suvam Chanda",
-  img:
-    "https://rukminim1.flixcart.com/image/416/416/ju5u1zk0/regionalbooks/n/w/e/taranath-tantrik-samagrah-original-imaffcfvctwa2etv.jpeg?q=70",
-};
-const secondBook = {
-  title: "Ikigai: The Japanese secret to a long and happy life Hardcover – 27",
-  author: "Héctor García",
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81l3rZK4lnL.SR160,240_BG243,243,243.jpg",
-};
-
+const books = [
+  {
+    title: "Taranath Tantrik",
+    author: "Taradas Bandopadhyay, Bibhutibhushan Bandopadhyay, Suvam Chanda",
+    img:
+      "https://rukminim1.flixcart.com/image/416/416/ju5u1zk0/regionalbooks/n/w/e/taranath-tantrik-samagrah-original-imaffcfvctwa2etv.jpeg?q=70",
+  },
+  {
+    title:
+      "Ikigai: The Japanese secret to a long and happy life Hardcover – 27",
+    author: "Héctor García",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81l3rZK4lnL.SR160,240_BG243,243,243.jpg",
+  },
+];
+const names = ["Suvam", "Rahul", "Avik"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 const Book = ({ img, title, author }) => {
   // props Destructuring
