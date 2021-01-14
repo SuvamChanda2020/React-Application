4addb2aadcd84abf3bcfe1dc0ebbe7ea13a35274
SuvamChanda2020 <suvam.net001@gmail.com>
2021-01-14 11:19:38 +0530

    Event Handler

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -41,17 +41,43 @@ function BookList() {
   );
 }
 const Book = ({ title, img, author }) => {
-  // props Destructuring
+  // attribute, eventHandlar
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    console.log("Hello World");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
 
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        {title}
+      </h1>
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
         {author}
       </h4>
+      <button type="button" onClick={clickHandler}>
+        refernce Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More Complex Example
+      </button>
     </article>
   );
 };
